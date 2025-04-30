5ed82656f52e395f40466fa23ba6c0cb4c246228
rcabotaje <130100627+rcabotaje@users.noreply.github.com>
2025-04-29 23:53:57 -0700

    num1

--- scripts/lab4.js
+++ scripts/lab4.js
@@ -6,7 +6,7 @@
  * @returns The sum of the two numbers if add is true and false otherwise.
  */
 function sumValues(num1, num2, add) {
-    if(typeof num != "number" || typeof num2 != "number" || typeof add != "boolean"){
+    if(typeof num1 != "number" || typeof num2 != "number" || typeof add != "boolean"){
         return false;
     }
     
